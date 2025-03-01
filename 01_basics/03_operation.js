c9c807ede3fc7530f36e16c56152b8af6d62c3e1
anshujain0 <jainanshu814@gmail.com>
2025-03-01 17:43:30 +0000

    comparison

---
+++ 01_basics/03_operation.js
@@ -0,0 +1,30 @@
+let value=5
+let negValue= -value
+console.log(negValue);
+
+// console.log(2+2);
+// console.log(2*2);
+// console.log(2**3);  //2 raise to power 3
+
+let str1="hello"
+let str2=" Anshu"
+let str3=str1+str2 //basic string addition
+console.log(str3); 
+
+console.log("1"+2);
+console.log(1+"2");
+
+console.log("1" + 2 + 2); //op-122 if string 1st then all 3 treated as string
+console.log(1 + 2 + "2"); //op-32 if initial are number added then string add
+
+console.log(true) //op true
+console.log(+true) //op 1 as conversion
+console.log(+"")
+
+
+let num1,num2,num3
+num1=num2=num3=2+2 //represting way or assignment
+
+let counter=100
+counter++; //++counter .. pre-increment and post increment
+console.log(counter);
---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,9 @@
+// console.log(2>1) true
+
+console.log("2">1)
+console.log("02">1)
+
+console.log(null > 0) // false
+console.log(null == 0) //false
+console.log(null >= 0) //true
+/* the equality == & comparison > < = works differently. Comparisons convert Null to a number treating as 0 */
