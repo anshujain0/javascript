aa4f755b5854507c6ec18bd924114f53ffcc8af5
anshujain0 <jainanshu814@gmail.com>
2025-03-01 13:57:56 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId=18728
+let accountEmail= "anshu@google.com"
+var accountPassword = "12345"
+accountCity = "Ujjain"
+let accountState;
+
+//accountId= 48278 //not allowed as constant are not changed after dec
+
+accountEmail= "aj@gmail.com"
+accountPassword= "34343"
+accountCity ="Indore"
+
+console.log(accountId);
+
+console.table([accountEmail, accountPassword, accountCity, accountState])
+
+/*prefer not to use var 
+because of block scope and functional scope */
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,14 @@
+"use strict"; //treat all JS code as newer version
+
+//alert (3 + 3) //we are using nodejs , not browser
+
+let name = "Anshu" //string
+let age=15 // number
+let Isloggedin = true //boolean
+
+console.log(typeof "Anshu");
+console.log(typeof age);
+console.log(typeof null) //object
+console.log(typeof undefined); //undefined
+
+//type of data types - string, boolean ,null, undefined,...
