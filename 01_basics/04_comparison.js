0b7a71a3651d63febf0851e8281713f24e7fbfa8
anshujain0 <jainanshu814@gmail.com>
2025-03-03 11:22:39 +0000

    datatype

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -7,3 +7,5 @@ console.log(null > 0) // false
 console.log(null == 0) //false
 console.log(null >= 0) //true
 /* the equality == & comparison > < = works differently. Comparisons convert Null to a number treating as 0 */
+
+console.log("2"===2) //false strict check
