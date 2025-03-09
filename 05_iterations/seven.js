3624862e6e09f53446c34022eee977b77eec4d4a
anshujain0 <jainanshu814@gmail.com>
2025-03-09 17:54:43 +0000

    reduce in js

---
+++ 05_iterations/seven.js
@@ -0,0 +1,28 @@
+const myNums =[1,2,3]
+
+// const total = myNums.reduce( function(acc,currval) {
+//     console.log(`acc : ${acc} and current value : ${currval}`)
+//     return acc+currval
+// },0 ) //0 is giving initial value to accumulator
+
+const total = myNums.reduce( (acc,curr) => acc+curr ,0 )
+console.log(total)
+
+const shoppingCart = [
+    {
+        item : "js" ,
+        price : 2999
+    },
+    {
+        item : "python" ,
+        price : 999
+    },
+    {
+        item : "data science" ,
+        price : 12999
+    }
+]
+
+const totalprice = shoppingCart.reduce( (acc,item) => acc+item.price , 0 )
+
+console.log(totalprice)
